6c38890a4959ff2fa58a2f4fc6dac50a78c559fe
dolcizia <54375308+dolcizia@users.noreply.github.com>
2023-12-26 20:25:47 -0600

    Implement pinia store

---
+++ src/store/ExpenseStore.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia';
+import Database from '@/database';
+import { Expense } from '@/models';
+
+const db = new Database();
+
+export const useExpenseStore = defineStore('expenseStore', {
+  state: (): { expenses: Expense[] } => ({
+    expenses: [],
+    // other relevant state properties...
+  }),
+  actions: {
+    async fetchExpenses() {
+      this.expenses = await db.getExpenses();
+    },
+    async addExpense(expense: Expense) {
+      const newExpense = await db.addExpense(expense);
+      this.expenses.push(newExpense);
+    },
+    // other actions like updateExpense, deleteExpense...
+  },
+});
